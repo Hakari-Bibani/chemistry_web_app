6f1651170e70379d2830115097600fb6f5e5ceae
Hakari Bibani <hakary.jalal33@gmail.com>
2024-10-30 14:55:52 +0300

    Create app.py

---
+++ app.py
@@ -0,0 +1,19 @@
+import streamlit as st
+from home import home
+from explanation import explanation
+from calculations import calculations
+from lab import lab
+
+# Sidebar Navigation
+st.sidebar.title("Navigation")
+page = st.sidebar.radio("Go to", ["Home", "Explanation", "Calculations", "Lab"])
+
+# Display the selected page
+if page == "Home":
+    home()
+elif page == "Explanation":
+    explanation()
+elif page == "Calculations":
+    calculations()
+elif page == "Lab":
+    lab()
