4da3e0f2ef30fcc5887a374f6abffe56a5844bb2
Hakari Bibani <hakary.jalal33@gmail.com>
2024-10-30 17:40:07 +0300

    Update home.py

--- home.py
+++ home.py
@@ -1,19 +1,10 @@
 import streamlit as st
 
 def home():
-    # Set the page configuration first
-    st.set_page_config(
-        page_title="Chemistry Web App",
-        page_icon="🧪",
-        layout="wide",
-        initial_sidebar_state="expanded"
-    )
-
-    # Rest of the home page content
-    st.title("🧪 Welcome to your Virtual Chemistry Lab 🧪")
-    st.write("Explore and experiment with various chemistry concepts and reactions.")
-
+    st.title("Welcome to your virtual chemistry learning environment!")
+    
     col1, col2 = st.columns(2)
+
     with col1:
         st.subheader("📚 What you can do here:")
         st.write("""
@@ -21,6 +12,7 @@ def home():
         - Watch simulated reactions
         - Learn chemistry concepts
         """)
+
     with col2:
         st.subheader("🔬 Getting Started:")
         st.write("""
@@ -28,23 +20,3 @@ def home():
         2. Follow the instructions
         3. Experiment and learn!
         """)
-
-    # Add a chemistry-themed image
-    st.image("images/chemistry_lab.png", use_column_width=True)
-# Home Page
-def home_page():
-    st.markdown("<div class='container'>", unsafe_allow_html=True)
-    st.title("Welcome to your virtual chemistry learning environment!")
-    st.markdown("""
-    📚 What you can do here:
-    - Perform chemical calculations
-    - Watch simulated reactions
-    - Learn chemistry concepts
-    
-    🔬 Getting Started:
-    1. Select a section on the left side
-    2. Follow the instructions
-    3. Experiment and learn!
-    """)
-    st.markdown("</div>", unsafe_allow_html=True)
-
